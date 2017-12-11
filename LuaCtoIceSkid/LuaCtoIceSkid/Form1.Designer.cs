e467921f403c967ece630dd393610af2807208f4
Rover656 <Rover656@users.noreply.github.com>
2017-12-11 16:19:02 +0000

    Add LuaC to IceC converter

---
+++ LuaCtoIceSkid/LuaCtoIceSkid/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace LuaCtoIceSkid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.In = new System.Windows.Forms.RichTextBox();
+            this.Out = new System.Windows.Forms.RichTextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // In
+            // 
+            this.In.Location = new System.Drawing.Point(12, 12);
+            this.In.Name = "In";
+            this.In.Size = new System.Drawing.Size(276, 364);
+            this.In.TabIndex = 0;
+            this.In.Text = "";
+            // 
+            // Out
+            // 
+            this.Out.Location = new System.Drawing.Point(335, 12);
+            this.Out.Name = "Out";
+            this.Out.Size = new System.Drawing.Size(286, 364);
+            this.Out.TabIndex = 1;
+            this.Out.Text = "";
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(274, 382);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(75, 23);
+            this.Convert.TabIndex = 2;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 417);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.Out);
+            this.Controls.Add(this.In);
+            this.Name = "Form1";
+            this.Text = "Lua C to Ice C";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox In;
+        private System.Windows.Forms.RichTextBox Out;
+        private System.Windows.Forms.Button Convert;
+    }
+}
+
---
+++ LuaCtoIceSkid/LuaCtoIceSkid/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuaCtoIceSkid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            string[] array = In.Text.Split("\r\n".ToCharArray());
+            for (int i = 0; i < array.Length; i++)
+            {
+                string script = array[i];
+
+                string[] srcs = script.Split(' ');
+
+                if (srcs[0] == "getglobal")
+                {
+                    string parameter = "";
+                    int o = 1;
+                    while (o < srcs.Length)
+                    {
+                        parameter += srcs[o] + " ";
+                        o++;
+                    }
+                    result += "SKID_globalSkId(SkidState, " + '"' + parameter.TrimEnd() + '"' + ");";
+                }
+                else if (srcs[0] == "getfield")
+                {
+                    string parameter = "";
+                    int o = 2;
+                    while (o < srcs.Length)
+                    {
+                        parameter += srcs[o] + " ";
+                        o++;
+                    }
+                    result += "SKID_getSkId(SkidState, " + srcs[1] + "," + '"' + parameter.TrimEnd() + '"' + ");";
+                }
+                else if (srcs[0] == "pushvalue")
+                {
+                    result += "SKID_SkIdvalue(SkidState, " + srcs[1] + ");";
+                }
+                else if (srcs[0] == "pushnumber")
+                {
+                    result += "SKID_hOwMaNySkIdS(SkidState, " + srcs[1] + ");";
+                }
+                else if (srcs[0] == "pushstring")
+                {
+                    string parameter = "";
+                    int o = 1;
+                    while (o < srcs.Length)
+                    {
+                        parameter += srcs[o] + " ";
+                        o++;
+                    }
+                    result += "SKID_pushSkId(SkidState, " + '"' + parameter.TrimEnd() + '"' + ");";
+                }
+                else if (srcs[0] == "call")
+                {
+                    result += "SKID_SkId(SkidState, " + srcs[1] + "," + srcs[2] + ");";
+                }
+                else if (srcs[0] == "pcall")
+                {
+                    result += "SKID_bpSkId(SkidState, " + srcs[1] + "," + srcs[2] + "," + srcs[3] + ");";
+                }
+                else if (srcs[0] == "setfield")
+                {
+                    string parameter = "";
+                    int o = 2;
+                    while (o < srcs.Length)
+                    {
+                        parameter += srcs[o] + " ";
+                        o++;
+                    }
+
+                    result += "SKID_setSkId(SkidState, " + srcs[1] + "," + '"' + parameter.TrimEnd() + '"' + ");";
+                }
+                else if (srcs[0] == "emptystack")
+                {
+                    result += "SKID_SkIdtop(0);";
+                }
+                else if (srcs[0] == "settop")
+                {
+                    result += "SKID_SkIdtop(SkidState, " + srcs[1] + ");";
+                }
+                else if (srcs[0] == "pushboolean")
+                {
+                    result += "//PUSHBOOLEAN IS NOT SUPPORTED BY STOCK ICE, PLEASE UPGRADE ICE AND THE CONVERTER IN ORDER TO USE IT.";
+                }
+                result += "\r\n";
+            }
+            Out.Text = result;
+        }
+    }
+}
